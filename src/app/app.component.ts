3b0324ada594f52bcb3dbd03465ceec384df8b05
aperezNWO <alejandro.perez.acosta@hotmail.com>
2023-06-10 01:01:06 -0500

    Aplicativo [MCSD - CONSULTA LOG] v[1.0.0.10] 1) Html Main Page Title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ export class AppComponent {
   //
   title             : string = '[MCSD - CONSULTA LOG]';
   appName           : string = "[MCSD - CONSULTA LOG]";
-  appVersion        : string = '1.0.0.9';
+  appVersion        : string = '1.0.0.10';
   runtimeVersion    : string = VERSION.full;
  }   
